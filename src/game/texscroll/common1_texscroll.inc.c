287b685c072fe4fb9c3cdcee8dfdf031bf4b2ac7
AllyTally <alexiatilde@gmail.com>
2020-08-04 20:51:38 -0300

    add transparent star model

--- src/game/texscroll/common1_texscroll.inc.c
+++ src/game/texscroll/common1_texscroll.inc.c
@@ -1,6 +1,9 @@
 #include "actors/common1.h"
 #include "actors/star/texscroll.inc.c"
+#include "actors/transparent_star/texscroll.inc.c"
 void scroll_textures_common1() {
 	scroll_actor_geo_star();
+
+	scroll_actor_geo_transparent_star();
 
 }
--- src/game/texscroll/common1_texscroll.inc.h
+++ src/game/texscroll/common1_texscroll.inc.h
@@ -1,2 +1,3 @@
+#include "actors/transparent_star/texscroll.inc.h"
 #include "actors/star/texscroll.inc.h"
 extern void scroll_textures_common1();
